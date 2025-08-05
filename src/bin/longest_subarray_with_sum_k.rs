d8325be404f6f172d1d963e1893b4bab5be6c27f
ShreyanshVishwakarma <shreyanshvishwakarma@outlook.in>
2025-08-05 23:17:37 +0530

    refactor longest subarray with sum function and update test array

--- src/bin/longest_subarray_with_sum_k.rs
+++ src/bin/longest_subarray_with_sum_k.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
+//
+//this only works for strictly positive and non-zero arrays
+//this is better solution . not the best
 fn longets_subarry_with_sum_k(arr: &[i32], k: i32) -> Vec<i32> {
     let mut map: HashMap<i32, i32> = HashMap::new();
     let mut vec: Vec<i32> = Vec::new();
@@ -7,10 +10,12 @@ fn longets_subarry_with_sum_k(arr: &[i32], k: i32) -> Vec<i32> {
     let mut max_length: i32 = 0;
     for (i, v) in arr.iter().enumerate() {
         sum += v;
-        map.insert(sum, i as i32);
+        // to handle zeros in array just dont update the map in case of same sum
+        map.entry(sum).or_insert(i as i32);
         println!("key :{sum} and value : {i} added");
         if sum == k {
             max_length = i as i32 + 1;
+            //this assumes there are no negative values in array , which is the case here
             last_index = i;
             continue;
         }
@@ -31,7 +36,7 @@ fn longets_subarry_with_sum_k(arr: &[i32], k: i32) -> Vec<i32> {
     vec
 }
 fn main() {
-    let arr = vec![1, 4, 6, 8, 5, 1, 1, 1, 3, 6, 8];
+    let arr = vec![1, 4, 6, 8, 5, 1, 0, 1, -1, 1, 1, 3, 6, 8];
     let vec = longets_subarry_with_sum_k(&arr, 3);
     println!("{vec:?}");
 }
